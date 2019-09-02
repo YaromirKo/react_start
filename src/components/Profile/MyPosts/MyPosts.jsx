25486ddef452c872d5afdb3f6c10783698d702ca
YaromirKo <34893823+YaromirKo@users.noreply.github.com>
2019-09-03 04:06:00 +0500

    new logic in component structure
    update readme
    reorganization of component structure
    add posts component logic

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post/Post';
+
+const MyPosts = () => {
+    return (
+        <div>
+            <textarea/>
+            <button>add post</button>
+            <Post/>
+        </div>
+    )
+};
+
+export default MyPosts;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,13 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
         <div className={s.content}>
             <div><img src="https://img.icons8.com/material/4ac144/256/user-male.png" alt=""/></div>
             <div className={s.item}>ava</div>
+            <MyPosts/>
         </div>
     )
 };
